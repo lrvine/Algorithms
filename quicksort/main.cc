670b35976a74bc92b081cdfed8ea833fa1a58e00
lrvine <tsungyeh@gmail.com>
2016-06-12 14:32:23 +0800

    Add quicksort implementation

---
+++ quicksort/main.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include "quicksort.cc"
+
+using namespace std;
+
+int main(int argc, char **argv ){
+
+  int *array;
+  int length;
+  string infilename;
+  string outfilename;
+
+  if(argc){
+    length = atoi(argv[1]);
+    infilename = argv[2];
+    outfilename = argv[3];
+  }
+  else{
+    length = 10000;
+    infilename = "QuickSort.txt";
+    outfilename = "Sorted.txt";
+  }
+
+  ifstream input;
+  input.open(infilename);
+  if(input.fail()){
+    cout<<"input error!"<<endl;
+    return 1;
+  }
+
+  ofstream output;
+  output.open(outfilename);
+
+  cout<<"start quicksort on file "<<infilename<<" with legnth "<<length<<endl;
+  array = new int[length];
+  for(int i=0;i<length; i++){
+    input>>array[i];
+  }
+
+  input.close();
+  
+  quicksort( array, length );
+
+  cout<<"end"<<endl;
+  for(int i=0;i<length; i++){
+    output<<array[i]<<endl;
+  }
+  output.close();
+  delete array;
+
+  return 0;
+}
---
+++ quicksort/quicksort.cc
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+void quicksort( int* array, int length ){
+	int pivot=0;
+	int PartitionLine=1;
+	int buf=0;
+	int middle=length/2;
+
+ 	if( (length == 0) || (length == 1 ) ) 
+ 	  return;
+ 	
+	//Sort first,last,and middle for median-of-three
+	//pretend the first at the midlde position
+	
+	if( array[middle]>array[0]){
+		buf=array[0];
+		array[0]=array[middle];
+		array[middle]=buf;
+	}
+	if( array[0]>array[length-1]){
+		buf=array[0];
+		array[0]=array[length-1];
+		array[length-1]=buf;
+	
+		if( array[middle]>array[0]){
+			buf=array[0];
+			array[0]=array[middle];
+			array[middle]=buf;
+		}
+	}
+	//position 0 would be the midian of three
+	pivot=array[0];
+
+  	for(int i=1; i<length ; i++){
+  	  if(array[i]<=pivot){
+  	    if(PartitionLine != i){
+  	      buf=array[i];
+  	      array[i]=array[PartitionLine];
+  	      array[PartitionLine]=buf;
+  	    }
+  	    PartitionLine++;
+  	  }
+  	}   
+	//cout<<"PartitionLine is "<<PartitionLine<<endl;
+	//cout<<" pivot is "<<pivot<<endl;
+  	array[0]=array[PartitionLine-1];
+  	array[PartitionLine-1]=pivot;
+
+	quicksort( array, PartitionLine-1 );
+	quicksort( array+PartitionLine, length-PartitionLine);
+}
+
