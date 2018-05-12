5f28b90f6c2dd94aba03f3124e0cd2e7aa812973
lrvine <tsungyeh@gmail.com>
2018-05-12 15:53:04 +0800

    Add travelling salesman with branch and bound

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Algorithms
 Basic algorithm implementation in C++ 
-* add tree traversal  06/05/2016
-* add quicksort       06/12/2016
-* add Dijkstra        07/19/2016
-* add A*              08/29/2016
-* add mergesort       05/08/2018
+* add tree traversal  		06/05/2016
+* add quicksort       		06/12/2016
+* add Dijkstra        		07/19/2016
+* add A*              		08/29/2016
+* add mergesort       		05/08/2018
+* add travelling salesman  	05/12/2018
---
+++ travelling_salesman/tsp.cc
@@ -0,0 +1,133 @@
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <limits>
+
+using namespace std;
+
+struct route {
+	vector<int> path;
+	vector<int> novisit;
+	long double cost;
+	long double bound;
+}; // branch and bound's path route structure
+
+struct comparer  
+{  
+	bool operator()(const route l, const route r)  {  return l.bound > r.bound;  }  
+};
+
+long double calbound (route t, double** distance){
+	long double m1;
+	long double m2;
+	long double minToGoal = numeric_limits<long double>::max();
+	long double current;
+	int novisits = t.novisit.size();
+	long double bound = 0;
+	
+	for(int i = 0; i < novisits; i++){
+		current = distance[t.novisit[i]][0];
+		if(current < minToGoal)
+			minToGoal = current;
+		m1 = distance[t.novisit[i]][t.path.back()];
+		m2 = numeric_limits<long double>::max();
+		for(int j = 0; j < novisits; j++){
+			if( i != j ){
+				current = distance[t.novisit[j]][t.novisit[i]];
+				if(current < m2){
+					if(current < m1){
+						m2 = m1;
+						m1 = current;
+					}else{
+						m2 = current;
+					}
+				}	
+			}
+		}
+		bound += (m1 + m2);
+	}
+	return ((bound/2) + minToGoal + t.cost);
+}
+
+
+void tsp( vector< pair<double,double> >& vets ){
+ 
+	double **distance;
+
+	int size = vets.size();
+
+  	distance = new double *[size];
+  	for( int i = 0; i < size ; i++){
+     		distance[i] = new double[size];
+  	}
+  
+  	for( int i = 0; i < size ; i++){
+  		for( int j = 0; j < size ; j++){
+      			distance[i][j] = sqrt( pow( (vets[i].first-vets[j].first) , 2)
+				       + pow( (vets[i].second - vets[j].second) , 2 ) );
+      			//cout<<distance[i][j]<<" ";
+    		}
+    		//cout<<endl;
+ 	}	
+
+
+	priority_queue< route, vector<route>, comparer > rpq;  
+  	for(int i = 1; i < size; i++){
+		route current;
+		current.path.push_back(i);
+		for(int j = 1; j < size; j++){
+			if(j != i)
+				current.novisit.push_back(j);
+		}
+		current.cost = distance[i][0];
+		current.bound = calbound( current , distance );
+		rpq.push(current);
+	}
+
+	route current;
+  	route next;
+
+	//calcaulate one path as current best to reduce initial run qeueu size
+  	long double best = 0;
+  	for(int i = 0; i < size; i++){
+		best += distance[i][(i+1)%size];
+  	} 
+  	//cout<<"initial best "<<best<<endl;
+
+	// branch and bound for shortest route
+	int novisitsize;
+	while( !rpq.empty() ){
+		current = rpq.top();
+		rpq.pop();
+		novisitsize = current.novisit.size();
+        	cout<<"current priority queue size "<<rpq.size()<<" unvisited cities in current path "<<novisitsize<<endl;
+		for(int i = 0; i < novisitsize; i++){
+		 	next = current;
+		 	next.path.push_back(current.novisit[i]);
+		 	next.novisit.erase(next.novisit.begin()+i);
+		 	next.cost = current.cost + distance[current.novisit[i]][current.path.back()];
+			if (novisitsize==2){
+				next.cost += distance[next.novisit.back()][next.path.back()];
+				next.cost += distance[next.novisit.back()][0];
+			 	if(next.cost<best){
+				 	best = next.cost;
+                           		cout<<" new best "<<best<<endl;
+                         	}
+				continue;
+		 	}
+			next.bound = calbound( next, distance );
+		 	if( next.bound < best )
+		     		rpq.push(next);
+		}
+	}
+	cout<<"The final best distance is "<<best<<endl;
+
+  	for(int i = 0; i < size; i++){
+		delete distance[i];
+	}
+	delete distance;
+}
